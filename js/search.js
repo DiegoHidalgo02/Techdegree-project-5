7f8703af1c149ae48dcf8e3fef3df7134dbfe38a
Diego Hidalgo <Hidalgo.moreno.diego@gmail.com>
2024-06-21 08:15:17 +0200

    Created function to search item on grid

--- js/search.js
+++ js/search.js
@@ -1,5 +1,41 @@
 const authorSearch = document.getElementById('authorSearch');
+let currentValue;
+
+let card = document.getElementById('1');
+let styleCard = window.getComputedStyle(card);
+let width = styleCard.getPropertyValue('width');
+console.log(width);
 
 authorSearch.addEventListener('keyup', e => {
-    let currentValue = e.target.value.toLowerCase();
+
+    currentValue = e.target.value.toLowerCase();
+
+    let landScapes = document.querySelectorAll('a.grid-item');
+
+  
+    landScapes.forEach(landScape => {
+
+        if (landScape.dataset.caption.toLowerCase().includes(currentValue)){
+
+            landScape.style.display = "block";
+
+            document.getElementById('gallery').style.gridTemplateColumns = `repeat(auto-fit, minmax(200px, ${width}))`;
+
+        }else{
+
+            landScape.style.display = "none";
+
+        }
+    })
+
 });
+
+
+
+
+// let prova = document.querySelector('.gallery');    
+
+// console.log(typeof prova);
+
+// prova.style.gridTemplateColumns = "500px";
+
